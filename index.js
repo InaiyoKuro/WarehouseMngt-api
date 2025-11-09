49af9fe6697d403576b072679817058a031b58c7
Luan Ngo <inaiyokuro1724@gmail.com>
2025-11-09 07:09:39 +0700

    Update db

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.use(express.json())
 app.use("/api/products", productRoutes)
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => console.log("Server Running")) 
+app.listen(PORT, () => console.log("Server Running", PORT)) 
--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,20 @@
-const mysql = require("mysql2")
+const mysql = require("mysql2/promise")
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0
 })
 
 
-db.connect((err) => {
-  if(err) console.log("Failed to connect to MySQL", err);
-  else console.log("Successfully connected to MySQL");
+// db.connect((err) => {
+//   if(err) console.log("Failed to connect to MySQL", err);
+//   else console.log("Successfully connected to MySQL");
 
-})
+// })
 
 module.exports = db
